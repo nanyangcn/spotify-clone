6515936ba3e3f660994216d64304e29167411f0d
Yang Nan <nanyangcn@gmail.com>
2023-08-04 20:58:42 +0300

    bug fixed

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import { FaPlay } from 'react-icons/fa';
 import { useUser } from '@/hooks/useUser';
 import useAuthModal from '@/hooks/useAuthModal';
 
@@ -43,13 +42,6 @@ const ListItem = ({ image, name, href }: ListItemProps) => {
       <p className="truncate py-5 font-medium">
         {name}
       </p>
-      {/* <div
-        className="absolute right-5 flex items-center justify-center
-        rounded-full bg-green-500 p-4 opacity-90 drop-shadow-md transition
-        hover:scale-110 group-hover:opacity-100"
-      >
-        <FaPlay className="translate-x-[1.5px] text-black" />
-      </div> */}
     </button>
   );
 };
